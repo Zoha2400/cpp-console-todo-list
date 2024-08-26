c5569e07b45bcf19c2797828326ce280d19c2ba0
Zoha2400 <vasv29834@gmail.com>
2024-08-26 23:41:03 +0500

    feat: added console commands

--- main.cpp
+++ main.cpp
@@ -40,8 +40,28 @@ int main() {
     Task exampleTask2 = {2, "math","Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod malesuada. Nullam ac erat ante. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla.", false};
 
 
+    cout << "\n\n";
+    cout << "Welcome to your task manager \n";
+    cout << "---------------------------\n";
+    cout << "Your tasks \n";
+
     printTask(exampleTask);
     printTask(exampleTask2);
+
+    cout << "\n \n";
+
+    string command;
+    cout << "Choose a command and type it\n"
+            "================================\n"
+            "a - add a task\n"
+            "c - complete\n"
+            "v - display a task's discription\n"
+            "d - delete a task\n"
+            "================================"
+    << endl;
+
+    cin >> command;
+    cout << command;
 
     return 0;
 }
