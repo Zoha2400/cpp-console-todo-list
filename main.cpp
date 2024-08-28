690e8c4197799e3ee1ebd8ed135962f16f193aec
Zoha2400 <vasv29834@gmail.com>
2024-08-29 01:24:23 +0500

    feat: ыыseparate function for commands

--- main.cpp
+++ main.cpp
@@ -38,42 +38,8 @@ void printTask(const Task& task) {
     std::cout << "+" << border << "+" << std::endl;
 }
 
-int main() {
-    Task exampleTask = {1, "math", "First task", false};
-    Task exampleTask2 = {2, "math","Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod malesuada. Nullam ac erat ante. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla.", false};
-
-    // Task in vector
-
-    tasks.push_back(exampleTask);
-    tasks.push_back(exampleTask2);
-
-    cout << "\n\n";
-    cout << "Welcome to your task manager \n";
-    cout << "---------------------------\n";
-    cout << "Your tasks \n";
-
-    printTask(exampleTask);
-    printTask(exampleTask2);
-
-    cout << "\n \n";
-
-    char command;
-    cout << "Choose a command and type it\n"
-            "================================\n"
-            "a - add a task\n"
-            "c - complete\n"
-            "v - display a task's discription\n"
-            "d - delete a task\n"
-            "t - list tasks\n"
-            "q - quit\n"
-            "================================"
-    << endl;
-  while (true) {
-        cout << "Enter command (a: add, c: complete, v: view, d: delete, t: list tasks ,q: quit): ";
-        cin >> command;
-        cin.ignore();
-
-        switch (command) {
+int task_switching(char command) {
+     switch (command) {
             case 'a': {
                 string taskName, taskDescription;
 
@@ -157,6 +123,51 @@ int main() {
                 cout << "Invalid command. Please try again." << endl;
             }
         }
+
+    return  1;
+}
+
+int main() {
+    Task exampleTask = {1, "math", "First task", false};
+    Task exampleTask2 = {2, "math","Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod malesuada. Nullam ac erat ante. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla.", false};
+
+    // Task in vector
+
+    tasks.push_back(exampleTask);
+    tasks.push_back(exampleTask2);
+
+    cout << "\n\n";
+    cout << "Welcome to your task manager \n";
+    cout << "---------------------------\n";
+    cout << "Your tasks \n";
+
+    printTask(exampleTask);
+    printTask(exampleTask2);
+
+    cout << "\n \n";
+
+    char command;
+    cout << "Choose a command and type it\n"
+            "================================\n"
+            "a - add a task\n"
+            "c - complete\n"
+            "v - display a task's discription\n"
+            "d - delete a task\n"
+            "t - list tasks\n"
+            "q - quit\n"
+            "================================"
+    << endl;
+  while (true) {
+        cout << "Enter command (a: add, c: complete, v: view, d: delete, t: list tasks ,q: quit): ";
+        cin >> command;
+        cin.ignore();
+
+        int start_commands =  task_switching(command);
+
+        if(start_commands == 0) {
+            return 0;
+        }
+
     }
 
     return 0;
